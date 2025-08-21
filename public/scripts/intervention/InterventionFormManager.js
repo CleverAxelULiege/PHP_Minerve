c5eda971f7bbbce26cb409121857530ccf3156ee
test <test>
2025-08-21 11:06:16 +0200

    add content editable

--- public/scripts/intervention/InterventionFormManager.js
+++ public/scripts/intervention/InterventionFormManager.js
@@ -405,13 +405,14 @@ export class InterventionFormManager {
             // Build container
             let messageContainer = document.createElement("div");
             messageContainer.classList.add("message_container");
+            messageContainer.setAttribute("data-id", id);
 
             if (isPublic) {
                 messageContainer.innerHTML = `
                 <div class="message_header">
                      <div class="message_index_edit_button">
                         <span>#${index + 1}</span>
-                        <button type="button" title="Éditer le message">
+                        <button id="edit_message_button" type="button" title="Éditer le message">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M535.6 85.7C513.7 63.8 478.3 63.8 456.4 85.7L432 110.1L529.9 208L554.3 183.6C576.2 161.7 576.2 126.3 554.3 104.4L535.6 85.7zM236.4 305.7C230.3 311.8 225.6 319.3 222.9 327.6L193.3 416.4C190.4 425 192.7 434.5 199.1 441C205.5 447.5 215 449.7 223.7 446.8L312.5 417.2C320.7 414.5 328.2 409.8 334.4 403.7L496 241.9L398.1 144L236.4 305.7zM160 128C107 128 64 171 64 224L64 480C64 533 107 576 160 576L416 576C469 576 512 533 512 480L512 384C512 366.3 497.7 352 480 352C462.3 352 448 366.3 448 384L448 480C448 497.7 433.7 512 416 512L160 512C142.3 512 128 497.7 128 480L128 224C128 206.3 142.3 192 160 192L256 192C273.7 192 288 177.7 288 160C288 142.3 273.7 128 256 128L160 128z"/></svg>
                         </button>
                      </div>
@@ -439,7 +440,7 @@ export class InterventionFormManager {
                 <div class="message_header">
                      <div class="message_index_edit_button">
                         <span>#${index + 1}</span>
-                        <button type="button" title="Éditer le message">
+                        <button id="edit_message_button" type="button" title="Éditer le message">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M535.6 85.7C513.7 63.8 478.3 63.8 456.4 85.7L432 110.1L529.9 208L554.3 183.6C576.2 161.7 576.2 126.3 554.3 104.4L535.6 85.7zM236.4 305.7C230.3 311.8 225.6 319.3 222.9 327.6L193.3 416.4C190.4 425 192.7 434.5 199.1 441C205.5 447.5 215 449.7 223.7 446.8L312.5 417.2C320.7 414.5 328.2 409.8 334.4 403.7L496 241.9L398.1 144L236.4 305.7zM160 128C107 128 64 171 64 224L64 480C64 533 107 576 160 576L416 576C469 576 512 533 512 480L512 384C512 366.3 497.7 352 480 352C462.3 352 448 366.3 448 384L448 480C448 497.7 433.7 512 416 512L160 512C142.3 512 128 497.7 128 480L128 224C128 206.3 142.3 192 160 192L256 192C273.7 192 288 177.7 288 160C288 142.3 273.7 128 256 128L160 128z"/></svg>
                         </button>
                      </div>
@@ -467,11 +468,30 @@ export class InterventionFormManager {
             messageContainer.querySelector(".author_name").textContent = `${msgAuthorFirstname} ${msgAuthorLastName}`;
             messageContainer.querySelector(".message_date").textContent = formattedDate;
 
+            const editMessageButton = messageContainer.querySelector("#edit_message_button");
+            editMessageButton.addEventListener("click", () => this.displayEditBoxMessage(editMessageButton.closest(".message_container")));
             this.DOM.messagesContainer.appendChild(messageContainer);
         });
     }
 
+    /**
+     * @param {HTMLElement} messageContainer 
+     */
+    displayEditBoxMessage(messageContainer) {
+        /**@type {HTMLElement} */
+        const msgContent = messageContainer.querySelector(".message_content");
+        msgContent.setAttribute("contenteditable", "true");
+        msgContent.focus();
 
+        const range = document.createRange();
+        range.selectNodeContents(msgContent);
+        range.collapse(false);
+
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+    }
 
     addBreadcrumbItem(text, value, container) {
         const item = document.createElement('div');
